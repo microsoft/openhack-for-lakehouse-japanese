dbeda1bd6fdb6ca068c5484be9c79e6bfc89b251
nakazax <nakazax>
2022-11-08 05:43:36 +0000

    作業途中の状態をコミット

---
+++ day1_with_answers/day1_c4__azure_data_factory.py
@@ -0,0 +1,53 @@
+# Databricks notebook source
+# MAGIC %md # Hack Day 1
+# MAGIC ## Challenge4. Azure Data FactoryとAzure Databricksの連携
+# MAGIC ### 本ノートブックの目的：Azure Data FactoryからAzure Databricksノートブックを実行する方法を学ぶ
+# MAGIC - Q1. Azure Data Factoryのパイプラインの設定を行ってください
+# MAGIC - Q2. Azure Data Factoryのパイプラインを実行してください
+# MAGIC - Q3. Azure Data Factoryのパイプラインの実行結果を確認してください
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Azure Data Factoryとは
+# MAGIC TBD
+# MAGIC 
+# MAGIC 参考リンク
+# MAGIC - TBD
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Q1. Azure Data Factoryのパイプラインの設定を行ってください
+# MAGIC - Azureポータル (https://portal.azure.com) からAzure Data Factoryの編集画面 (スタジオ) にアクセスする
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC - Azure Data Factoryスタジオでパイプラインを作成する
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Q2. Azure Data Factoryのパイプラインを実行してください
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Q3. Azure Data Factoryのパイプラインの実行結果を確認してください
+
+# COMMAND ----------
+
+

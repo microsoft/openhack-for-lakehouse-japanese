aa5d5f51ac623d2e786142ce8421ad06f93a6a69
nakazax <nakazax>
2022-11-09 03:47:25 +0000

    ノートブックの骨組みを一旦保存

---
+++ day1_with_answers/day1_c5__powerbi.py
@@ -0,0 +1,19 @@
+# Databricks notebook source
+# MAGIC %md # Hack Day 1
+# MAGIC ## Challenge5. Microsoft Power BI と Azure Databricks の連携
+# MAGIC ### 本ノートブックの目的：Power BI レポートから Databricks SQL ウェアハウスに接続して視覚化する方法を学ぶ
+# MAGIC - Q1.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC <h3>受講者向けのガイダンス</h3>
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Microsoft Power BI とは
+# MAGIC TBD
+# MAGIC 
+# MAGIC 参考リンク
+# MAGIC TBD

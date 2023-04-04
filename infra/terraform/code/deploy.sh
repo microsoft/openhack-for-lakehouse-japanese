6916e1234e8590f74157c54e94a6438aff32cb20
ryoma.nagata <ryoma.nagata0713.b@gmail.com>
2023-04-04 10:17:07 +0900

    - ループ内のterraform apply をバックグラウンド実行に変更 - ログ出力を追加 - その他微修正

--- infra/terraform/code/deploy.sh
+++ infra/terraform/code/deploy.sh
@@ -5,38 +5,58 @@ set -o pipefail
 set -o nounset
 # set -o xtrace # For debugging
 
+mode="" #"plan"
+
+##################################################
+
 bash ./code/init.sh
 
 start=$(date)
+datetime=$(date "+%Y%m%d_%H%M%S")
+mkdir -p .logs 
 
-az account set --subscription $ARM_SUBSCRIPTION_ID
-
-echo "use subscription ${ARM_SUBSCRIPTION_ID}" 
+az account set --subscription $TRAINER_ARM_SUBSCRIPTION_ID
+echo "use subscription ${TRAINER_ARM_SUBSCRIPTION_ID}" 
 
 echo "terraform initializing..."
-
 terraform init
 
 # trainer
+trainer_logfile=".logs/iac_setup_trainer_${datetime}.log"
 
-echo "terraform setup for trainer..."
-
-terraform apply --auto-approve -var-file ./environments/dev/trainer.tfvars 
-mv ./terraform.tfstate ./terraform_trainer.tfstate
+if [ "${mode}" = "plan" ];then
+    echo "terraform plan for trainer..."
+    terraform plan -var-file ./environments/dev/trainer.tfvars -state=./terraform_trainer.tfstate >>${trainer_logfile} 2>&1
+else
+    echo "terraform apply for trainer..."
+    terraform apply --auto-approve -var-file ./environments/dev/trainer.tfvars -state=./terraform_trainer.tfstate >>${trainer_logfile} 2>&1
+fi
 
 # student
 
 echo "terraform setup for students..."
 
+az account set --subscription $STUDENT_ARM_SUBSCRIPTION_ID
+echo "use subscription ${STUDENT_ARM_SUBSCRIPTION_ID}" 
+
+echo "terraform initializing..."
+terraform init
+
 for i in `cat ./code/student_rg_list.txt`;
 do
-    echo "terraform initializing..."
-    terraform init
-    echo "${i}..."
-    terraform apply --auto-approve -var-file ./environments/dev/student.tfvars -var rg_name=$i
-    mv ./terraform.tfstate ./terraform_${i}.tfstate
+    student_logfile=".logs/iac_setup_student_${i}_${datetime}.log"
+    if [ "${mode}" = "plan" ];then
+        echo "terraform plan for student_${i}..."
+        terraform plan -var-file ./environments/dev/student.tfvars -var rg_name=$i -state=./terraform_${i}.tfstate >>${student_logfile} 2>&1 &
+    else
+        echo "terraform apply student_${i}..."
+        terraform apply --auto-approve -var-file ./environments/dev/student.tfvars -var rg_name=$i -state=./terraform_${i}.tfstate >>${student_logfile} 2>&1 &
+    fi
+    sleep 10
 done
 
+end=$(date)
+
 echo "Starting deployment at ${start}"
-echo "finish deployment at "$(date)
+echo "finish deployment at ${end}"
 
